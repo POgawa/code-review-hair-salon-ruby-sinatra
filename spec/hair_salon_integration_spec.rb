b804ad5ea4a07cb35c7b7efc515201a4f5787cbc
Paul Ogawa <paul.ogawa7@gmail.com>
2015-05-08 14:15:42 -0700

    It adds integration spec to add new stylist all tests passing

--- spec/hair_salon_integration_spec.rb
+++ spec/hair_salon_integration_spec.rb
@@ -1,6 +1,15 @@
 require('capybara/rspec')
 require('./app')
 require "spec_helper"
-
 Capybara.app = Sinatra::Application
-set(:show_exceptions,false)
+set(:show_exceptions, false)
+
+describe('creating a stylist', {:type => :feature}) do
+  it('processes user entry to create stylist') do
+    visit'/'
+    click_link'Add a new styist'
+    fill_in('name', :with => 'Wanda')
+    click_button('Enter new stylist')
+    expect(page).to have_content('Wanda')
+  end
+end
