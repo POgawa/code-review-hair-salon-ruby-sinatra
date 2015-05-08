7d008da228487bf05efc40e58d59857d56cd022d
Paul Ogawa <paul.ogawa7@gmail.com>
2015-05-08 11:05:30 -0700

    It adds a delete functionality to client

--- lib/client.rb
+++ lib/client.rb
@@ -35,13 +35,13 @@ class Client
      @name = attributes.fetch(:name)
      DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{@id};")
      @stylist_id = attributes.fetch(:stylist_id)
-     DB.exec("UPDATE clients SET stylist_id = '#{@stylist_id}' WHERE id = #{@id};")
+    #  DB.exec("UPDATE clients SET stylist_id = '#{@stylist_id}' WHERE id = #{@id};")
    end
 
 
    def delete
      DB.exec("DELETE FROM clients WHERE id = #{@id};")
-     DB.exec("DELETE FROM stops WHERE stylist_id = #{@id}")
+     
    end
 
 
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,20 +1,44 @@
 require "spec_helper"
 
-describe(Stylist) do
+describe(Client) do
 
   describe("#==") do
-    it("is true for tasks with the same attributes") do
-        task1 = Stylist.new({:name => "Jennifer", :id => nil, :stylist_id => 1})
-        task2 = Stylist.new({:name => "Jennifer", :id => nil, :stylist_id => 1})
-        expect(task1).to eq(task2)
+    it("is true for clients with the same attributes") do
+        client1 = Client.new({:name => "Jennifer", :id => nil, :stylist_id => 1})
+        client2 = Client.new({:name => "Jennifer", :id => nil, :stylist_id => 1})
+        expect(client1).to eq(client2)
       end
     end
 
   describe("#save") do
-    it("adds the task to the tasks table") do
-      test_task = Stylist.new({:name => "Jennifer", :id => nil, :stylist_id => 1})
-      test_task.save()
-      expect(Stylist.all()).to(eq([test_task]))
+    it("adds the client to the clients table") do
+      test_client = Client.new({:name => "Jennifer", :id => nil, :stylist_id => 1})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
     end
   end
+
+  describe("#delete") do
+      it("deletes a client from the clients table") do
+        client1 = Client.new({:name => 'Jennifer', :id => nil, :stylist_id => 1})
+        client1.save()
+        client2 = Client.new({:name => 'Carrie', :id => nil, :stylist_id => 1})
+        client2.save()
+        client1.delete()
+        expect(Client.all()).to(eq([client2]))
+      end
+    end
+
+    describe("#update") do
+      it("updates the client object's name") do
+        client1 = Client.new({:name => 'Jennifer', :id => nil, :stylist_id => 1})
+        client1.save()
+        client1.update({:name => 'Jen'})
+        expect(client1.name()).to(eq('Jen'))
+      end
+
+end
+
+
+
 end
